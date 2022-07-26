300f61a95c713c961c4ed263cb05b566a66575fb
metastable-void <git@yuka.h09.eu>
2022-07-26 21:54:30 +0900

    reduced stun servers

--- public/assets/main.js
+++ public/assets/main.js
@@ -1091,19 +1091,10 @@ const createCall = async (base64PublicKey, selfInitiated) => {
   const configuration = {
     iceServers: [
       {
-        urls: 'stun:stun.l.google.com:19302',
-      },
-      {
         urls: 'stun:stun1.l.google.com:19302',
       },
       {
         urls: 'stun:stun2.l.google.com:19302',
-      },
-      {
-        urls: 'stun:stun3.l.google.com:19302',
-      },
-      {
-        urls: 'stun:stun4.l.google.com:19302',
       },
     ],
   };
